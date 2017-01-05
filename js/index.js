0d865889bce4d1fae035cb6ea93a9e8e7a73b1f4
pengteling <pengteling@gmail.com>
2017-01-06 01:06:49 +0800

    react webpack

---
+++ js/index.js
@@ -0,0 +1,56 @@
+// var React = require('react');
+// console.log("t");
+// import React from 'react';
+// import Router from 'react-router';
+
+//var React = require("react");
+//var ReactDOM = require("react-dom");
+import React from 'react';
+import ReactDOM from 'react-dom';
+var Hello = React.createClass({
+	getInitialState() {
+		console.log("getInitialState");
+		return{
+			tit:"getInitialState"
+		}
+
+	},
+	getDefaultProps() {
+		return{
+			prop1:"123"
+		}
+	},
+	componentWillUpdate(nextProps, nextState) {
+		//this.state.tit="componentWillUpdate";
+		console.log("componentWillUpdate" + nextProps + "," +nextState.tit);
+	},
+	componentWillMount() {
+		//this.state.tit="componentWillMount";
+		//this.refs.tips.innerHTML="test";
+		console.log("componentWillMount");
+	},
+	componentDidMount() {
+		
+		console.log("componentDidMount");
+		
+	},
+	componentDidUpdate(prevProps, prevState) {
+		console.log("componentDidUpdate"+prevState.tit);
+	},
+	componentWillUnmount() {
+		console.log("componentWillUnmount");
+	},
+	handleClick:function(){
+		this.setState({tit:"handleClick"});
+		console.log("handleClick");
+	},
+	render:function(){
+		return (
+			<div><h1 onClick={this.handleClick}>{this.props.title} - {this.state.tit}</h1><div ref="tips">test</div></div>
+			)
+	}
+});
+ReactDOM.render(
+        <Hello title="ptl"/>,
+        document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+/*
+cnpm i webpack webpack-dev-server jsx-loader react-hot-loader style-loader css-loader --dev-save
+
+*/
+var webpack = require('webpack');
+
+
+module.exports = {
+  // entry: './js/entry.js',
+  entry: [
+      //'webpack-dev-server/client?http://127.0.0.1:8080', // WebpackDevServer host and port
+      //'webpack/hot/only-dev-server',
+    './js/index.js' // Your app始s entry point
+  ],
+  output: {
+    path: __dirname + '/js/',
+    filename: 'bundle.js',
+    publicPath: '/js/'
+  },
+  module: {
+    loaders: [    
+    {
+      test: /\.js$/,
+      loader: 'babel',
+     // exclude: /node_modules/,  
+      query: {
+        presets: ['es2015', 'react']        
+      }
+    }
+
+    ]
+  },
+  
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    //new webpack.NoErrorsPlugin()
+  ]
+};
