f3d2bcee9a08213c187683187e79c9ba56ebaf13
pengteling <pengteling@gmail.com>
2017-01-10 01:51:21 +0800

    20170110

--- webpack.config.js
+++ webpack.config.js
@@ -6,48 +6,43 @@ var webpack = require('webpack');
 
 
 module.exports = {
-  // entry: './js/entry.js',
-  entry: [
-      //'webpack-dev-server/client?http://127.0.0.1:8080', // WebpackDevServer host and port
-      //'webpack/hot/only-dev-server',
-    //'./js/index.js' // Your app始s entry point
-    './src/components/GalleryByReactApp.js'
-  ],
-  output: {
-    path: __dirname + '/js/',
-    filename: 'bundle.js',
-    publicPath: '/js/'
-  },
-  module: {
-    loaders: [    
-    {
-      test: /\.js$/,
-      loader: 'babel',
-     // exclude: /node_modules/,  
-      query: {
-        presets: ['es2015', 'react']        
-      }
+    // entry: './js/entry.js',
+    entry: [
+        //'webpack-dev-server/client?http://127.0.0.1:8080', // WebpackDevServer host and port
+        //'webpack/hot/only-dev-server',
+        //'./js/index.js' // Your app始s entry point
+        './src/components/GalleryByReactApp.js'
+    ],
+    output: {
+        path: __dirname + '/js/',
+        filename: 'bundle.js',
+        publicPath: '/js/'
     },
-    {
-                test: /\.scss$/,                
-                loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version", "> 1%"]}!sass?sourceMap'
-    }
-    ,{
-                test: /\.css$/,
-                loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version", "> 1%"]}'
-     },
-     {
-              test: /\.json$/,
-              loader: 'json-loader'
-     }, {
-                test: /\.(png|jpe?g|gif|eot|svg|ttf|woff2?)$/,
-                loader: "url-loader?limit=8192"
-      }
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel',
+            // exclude: /node_modules/,  
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }, {
+            test: /\.scss$/,
+            loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version", "> 1%"]}!sass?sourceMap'
+        }, {
+            test: /\.css$/,
+            loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version", "> 1%"]}'
+        }, {
+            test: /\.json$/,
+            loader: 'json-loader'
+        }, {
+            test: /\.(png|jpe?g|gif|eot|svg|ttf|woff2?)$/,
+            loader: "url-loader?limit=8192"
+        }]
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        //new webpack.NoErrorsPlugin()
     ]
-  },
-  
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    //new webpack.NoErrorsPlugin()
-  ]
 };
