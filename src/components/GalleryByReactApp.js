23e8a69457674593c4491427558411dd57c156e3
pengteling <pengteling@gmail.com>
2017-01-11 21:02:40 +0800

    wrong is end

--- src/components/GalleryByReactApp.js
+++ src/components/GalleryByReactApp.js
@@ -16,34 +16,94 @@ var imageDatas = imageDatas_all.map(function(val){
 		desc: val.desc
 	};
 });
-var ImgFigure = React.createClass({
-	
-	handleClick:function(){
+var ImgFigure = React.createClass({		
+	layoutImage:function(){		
+		var stageW = $(window).width();
+		var stageH = $(window).height();
+		var halfStageW = Math.ceil(stageW/2);
+		var halfStageH = Math.ceil(stageH/2);
+		var rndx =0,rndy=0,rndrotate=0;		
+			
+		if(this.props.index==this.props.centerindex){ 
+			//如果中心图片
+			//全部为0
+			//console.log("当前中心图片id"+this.props.index);
+			rndx = halfStageW -120;
+			rndy = halfStageH -120;
+			rndrotate = 0;
+		}
+		else{
+				//rndx = Math.random() * stageW;
+			rndx = (Math.random() -0.5>0) ? (Math.random()*halfStageW  - 360) : (Math.random()*halfStageW  + halfStageW + 360);
+			rndy =Math.random()* stageH;
+			rndrotate = (0.5 - Math.random())* 60;			
+		}
+
+		return {			
+				left: rndx +"px",
+				top: rndy +"px",
+				rotate : rndrotate +"deg"			
+		};
+	},
+	componentWillMount() {
+		//this.layoutImage();
+		//console.log("子组件的 componentWillMount")
 		
-		// if(this.props.centerindex == this.props.index){
-		// 	if(this.state.classnamestr!="isback"){
-		// 		this.setState({
-		// 			classnamestr : "isback"
-		// 		});				
-		// 	}	
-		// 	else{
-		// 		this.setState({
-		// 			classnamestr : ""
-		// 		});
-		// 	}
-		// 	//this.props.dolayout(this.props.index); //父组件里面的事件
+	},	
+	componentWillUpdate(nextProps, nextState) {  //state未更新前。。。。
+		// if(nextState == this.state  ){ //子组件的state 未发生变化 即只是父组件传过来的参数变化 centerindex 
+		// 	//this.layoutImage();
+		// 	//console.log(nextProps);
+		// 	//if(this.props.index==1)
+		// 	//console.log("当前中心图片id" + this.props.centerindex + "当前图片id" + this.props.index);
+		// 	// if(this.props.index==this.props.centerindex){ 
+		// 	// 	console.log("当前中心图片id" + this.props.centerindex +"当前图片id" + this.props.index);
+		// 	// 	console.log("当前图片位置：" + this.state.pos.left);
+		// 	// }
+		// 	 if(this.props.index==this.props.centerindex){ 
+		// 	console.log("当前中心图片id" + this.props.centerindex +"当前图片id" + this.props.index+"当前图片位置：" + this.state.pos.left);
+				
+		// 	 }
 		// }
-		// else{
-		// 	//清除一下已经翻转的状态
-		// 	$(this.refs.imgC).siblings().removeClass("isback"); //已经翻转的 要翻转回去
-		// 	this.props.dolayout(this.props.index); //父组件里面的事件
-		// } className={this.state.classnamestr}
-		this.props.dolayout(this.props.index, !this.props.isback);
-		//console.log("当前点击ID："+this.props.index);
+	},
+	componentDidUpdate(prevProps, prevState) {
+		/*如果引起变化 是props引起 而不起state引起的 */
+		if(prevState == this.state  ){
+				var imageDom = this.refs.imgC;
+				$(imageDom).css(this.layoutImage()); //重新排位			
+		}
+		//var imageDom = this.refs.imgC;
+		//$(imageDom).css(this.state.pos);
+		if(this.props.index==this.props.centerindex){ 
+				console.log("当前中心图片id" + this.props.centerindex +"当前图片id" + this.props.index);
+
+		}else{
+
+		}
+
+
+	},
+	componentDidMount() {
+		var imageDom = this.refs.imgC;
+		$(imageDom).css(this.layoutImage());
+		//console.log(this.layoutImage());
+	},
+	handleClick:function(e){
+		if(this.props.index==this.props.centerindex){
+			this.props.doback();
+		 }
+		else{
+			//this.layoutImage();
+			//调用父组件
+			//this.props.doback();
+			this.props.dolayout(this.props.index);
+
+			//this.layoutImage();
+		}	
 	},
 
 	render() {
-
+		//console.log("render 子组件");
 		var classnamestr ="";
 		if(this.props.index == this.props.centerindex ){
 			classnamestr = this.props.isback ? 'isback' : '';
@@ -68,71 +128,33 @@ var GalleryByReactApp = React.createClass({
 			isback: false //正面还是背面？
 		}
 	},	
-	reLayoutImage: function(centerindex ,isback){
-		if(this.state.img_center_index != centerindex){
+	reLayoutImage: function(centerindex ){		
 			this.setState({
-				img_center_index: centerindex,
-				isback:false
-			});
-			console.log("点击居中ID："+ centerindex);
-			//this.layoutImage();
-
-		}
-		else{
-			this.setState({
-				isback : !this.state.isback
-			});
-		}
-		
-	},
-	layoutImage:function(){		
-		
-		var stageDOM = this.refs.stage;
-		var stageW = $(stageDOM).width();
-		var stageH = $(stageDOM).height();
-		var halfStageW = Math.ceil(stageW/2);
-		var halfStageH = Math.ceil(stageH/2);
-
-		//console.log(halfStageH);
-		//console.log(halfStageW);
-		//console.log("当前居中ID："+this.state.img_center_index); //当前居中ID
-
-		var centerimg =ReactDOM.findDOMNode(this.refs['img'+this.state.img_center_index]);
-			//console.log(centerimg);
-		$(centerimg).css("left",(halfStageW - 120)+'px').css("top", halfStageH - 135 +'px').css("z-index",3).css("rotate","0deg").siblings().css("z-index",1);
-
-		var centernav =ReactDOM.findDOMNode(this.refs['nav'+this.state.img_center_index]);
-		$(centernav).addClass("active").siblings().removeClass("active");
-
-			var rndx =0,rndy=0,rndrotate=0;
-			imageDatas.map(function(val,key){
-				//console.log(this.state.img_center_index);
-				if (key!= this.state.img_center_index){
-					//rndx = Math.random() * stageW;
-					rndx = (Math.random() -0.5>0) ? (Math.random()*halfStageW  - 360) : (Math.random()*halfStageW  + halfStageW + 360);
-					rndy =Math.random()* stageH;
-					rndrotate = (0.5 - Math.random())* 60;
-					$(ReactDOM.findDOMNode(this.refs['img'+key])).css("left",rndx+"px").css("top",rndy+"px").css("rotate",rndrotate+"deg");
-				}
-			}.bind(this));
-
+				img_center_index: centerindex
+			});					
+	},	
+	goback: function(){
+		this.setState({
+			isback : !this.state.isback
+		})
 	},
 	//组件加载以后 为每张图片计算位置范围
 	componentDidMount() {
-		this.layoutImage();
+		//this.layoutImage();
+		//console.log("重新加载")
 	},	
 	componentDidUpdate(prevProps, prevState) {
 		//this.layoutImage();
 	},
 	render() {
-		console.log("render");
+		console.log("render 父组件");
 
 		var controllerUnits =[];
 		var ImgFigures = [];
 		//console.log(imageDatas);
 		imageDatas.map(function(val,key){
 			//console.log(key);
-			ImgFigures.push(<ImgFigure key={key} index={key} centerindex={this.state.img_center_index} src={val.filename} ref={'img'+key} tit={val.title} desc={val.desc} isback={this.state.isback} dolayout={this.reLayoutImage} />);
+			ImgFigures.push(<ImgFigure key={key} index={key} centerindex={this.state.img_center_index} src={val.filename} ref={'img'+key} tit={val.title} desc={val.desc} dolayout={this.reLayoutImage} isback={this.state.isback} doback={this.goback}/>);
 		
 			controllerUnits.push(<ControllerUnits key={key} index={key} ref={'nav'+key}  centerindex={this.state.img_center_index} dolayout={this.layoutImage}  />);
 		}.bind(this));
