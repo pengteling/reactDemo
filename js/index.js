63eab7a0ae8260e252ff40ffa92129f0bce7ab3e
pengteling <pengteling@gmail.com>
2017-01-18 01:10:11 +0800

    home

--- js/index.js
+++ js/index.js
@@ -17,22 +17,30 @@ class MyCss3Animation  extends React.Component{
 		super(props)	
 		this.handleClick = this. handleClick.bind(this)	
 		this.state={
-			ishid:false
+			ishid:false,
+			index:Math.random()
 		}
 	}
 	handleClick(){
 		this.setState({
-			ishid :!this.state.ishid
+			ishid :!this.state.ishid,
+			index:Math.random()
 		})
 		
 	}
 	render(){
 		//var ani = this.state.ishid?  <h3 key="123">中国人</h3> : <h3 key="1">2</h3>
+		var css = this.state.ishid? { display:"none"} : {display:"block"}
+		var html = (
+			<div key={this.index}  style={css}>
+			test
+			</div>
+			)
 		return(
 			<div>
 				<button onClick={this.handleClick}>点击</button>
-				<ReactCSSTransitionGroup transitionName = "animation" key="ptl" >
-					
+				<ReactCSSTransitionGroup transitionName = "animate" transitionEnterTimeout={5000} transitionLeave={true} >
+					{html}
 				</ReactCSSTransitionGroup>
 			</div>
 			)
