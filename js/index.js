aa491512c03755b773f1801d5872a75fb1e054d2
pengteling <pengteling@gmail.com>
2017-02-07 21:30:21 +0800

    router

--- js/index.js
+++ js/index.js
@@ -7,106 +7,73 @@
 //var ReactDOM = require("react-dom");
 import React from 'react';
 import ReactDOM,{findDOMNode} from 'react-dom';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
 //import $ from 'jquery';
 require("./../sass/main.scss");
 
-import Perf from 'react-addons-perf';
-import Immutable from 'immutable';
-//import update from 'react-addons-update'; // ES6 
-import update from 'immutability-helper';
-//import PureRenderMixin from 'react-addons-pure-render-mixin';
-const __DEV__  = true ;
 
-if(__DEV__){
-    window.Perf = Perf 
-}
 
-class SurveyList extends React.PureComponent{
-    constructor(props) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-        this.state= {
-            items:Immutable.fromJS([
-            {
-                id :0,
-                text:"你喜欢吃萝卜吗",
-                on:"喜欢",
-                off:"不喜欢",
-                checked:false
-            },
-            {
-                id :1,
-                text:"你喜欢吃西瓜吗",
-                on:"喜欢",
-                off:"不喜欢",
-                checked:false
-            },
-            {
-                id :2,
-                text:"你喜欢吃香蕉吗",
-                on:"喜欢",
-                off:"不喜欢",
-                checked:false
-            }
-            ])
-        }
-    }
-    handleChange(labelId){      
-       
-        this.setState({
-            items: this.state.items.setIn([labelId,"checked"], !this.state.items.getIn([labelId,"checked"]))
-            //items: this.state.items
-        })
-        
-    }
-    componentDidUpdate() {
-        console.log('did update');
-      }
-    render(){
-        let that = this;
-        
-        return(
-            <div>
-            {
-                this.state.items.map(label=>{
-                    //console.log(i)
-                    return <Checkbox label={label} key={label.get("id")} onChange={that.handleChange.bind(that,label.get("id"))} />
-                })
-            }
-            </div>
-            )
-    }
-}
+const BasicExample = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/topics">Topics</Link></li>
+      </ul>
 
-class Checkbox extends React.PureComponent{
-    constructor(props) {
-        super(props); 
-        this.handleChange = this.handleChange.bind(this);       
-        //this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);  //PureRenderMixin
+      <hr/>
 
-    }
-    handleChange(){
-        //this.props.onChange(this.props.label.id);
-        this.props.onChange();
-    }
-    componentDidUpdate() {
-        console.log('did update');
-      }
-    render(){
-        //console.log(this.props.label.get("id"))
-        return(
-            <div>
-            {this.props.label.get("text")}
-            <input type="checkbox" checked={this.props.label.get("checked")} onChange={this.handleChange} />
-            {this.props.label.get("checked")? this.props.label.get("on") : this.props.label.get("off")}
-            </div>
-            )
-    }
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/topics" component={Topics}/>
+    </div>
+  </Router>
+)
 
-}
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+const Topics = ({ match }) => (    
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li><Link to={`${match.url}/rendering`}>Rendering with React</Link></li>
+      <li><Link to={`${match.url}/components`}>Components</Link></li>
+      <li><Link to={`${match.url}/props-v-state`}>Props v. State</Link></li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic}/>
+    <Route exact path={match.url} render={() => (
+      <h1>Please select a topic.</h1>
+    )}/>
+  </div>
+)
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+)
+
 
 
 ReactDOM.render(
-    <SurveyList />,
+    <BasicExample />,
     document.getElementById('app')
 );
